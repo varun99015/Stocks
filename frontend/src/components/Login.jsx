3983632b3a07bf6f2a5a31ed78a25ff7ebcc8795
varun99015 <153973261+varun99015@users.noreply.github.com>
2025-08-16 20:43:14 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
         }
 
         try {
-            const response = await fetch("http://localhost:5000/login", {
+            const response = await fetch("https://stocks-backend-fdcd.onrender.com/login", {
                 method: "POST",
                 credentials: "include",
                 headers: { "Content-Type": "application/json" },

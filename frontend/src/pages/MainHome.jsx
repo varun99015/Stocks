99a386a40b3c3ba608da5eb5c3b77bcb36a675aa
varun99015 <153973261+varun99015@users.noreply.github.com>
2025-08-20 12:08:45 +0530

    Update MainHome.jsx

--- frontend/src/pages/MainHome.jsx
+++ frontend/src/pages/MainHome.jsx
@@ -1,24 +1,21 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-//import Navbar from "../components/Navbar";
 import Explore from "./Explore";
-import Stocks from "./Stocks";
 import SpaceX from "./SpaceX";
 import Profile from "../components/Profile";
 import Dashboard from "./Dashboard";
 import Footer from "../components/Footer";
+import MarketOverview from "../components/MarketOverview";
 
 const MainHome = () => {
   return (
     <div>
-      {/* Routes for Dashboard, Stocks, etc. */}
       <Routes>
         <Route path="explore" element={<Explore />} />
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path="stocks" element={<Stocks />} />
         <Route path="spacex" element={<SpaceX />} />
         <Route path="profile" element={<Profile />} />
         <Route path="footer" element={<Footer/>} />
+        <Route path="marketoverview" element={<MarketOverview/>} />
       </Routes>
     </div>
   );

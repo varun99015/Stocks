e55145a278f16437b556d2b884616046ef6b5813
varun99015 <153973261+varun99015@users.noreply.github.com>
2025-08-20 11:59:53 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,7 @@
-import React,{useState,useEffect} from 'react';
+import {useState,useEffect} from 'react';
 import { Link } from "react-router-dom";
 import { ReactTyped as Typed } from 'react-typed';
-import video1 from '../assets/video1.mp4';
-import video2 from '../assets/video2.mp4';
+import video1 from "../assets/stocks.mp4";
 
 const Hero = () => {
     const [mounted, setMounted] = useState(false);
@@ -12,7 +11,7 @@ const Hero = () => {
     }, []);
   
     return (
-            <div className="flex flex-col items-center mt-6 lg:mt-20 bg-space-1 bg-cover bg-center bg-no-repeat h-screen min-h-[1000px] ">  
+            <div className="flex flex-col items-center mt-6 lg:mt-20 bg-space-1 bg-cover bg-center bg-no-repeat h-auto">  
                 
                 <div className="mx-auto max-w-5xl py-32 sm:py-38 lg:py-20 text-center">
                  
@@ -24,13 +23,12 @@ const Hero = () => {
                         {mounted && ( 
             <Typed className='bg-gradient-to-t from-orange-700 to-red-900 text-transparent bg-clip-text'
               strings={[
-                "SpaceX",
-                "ISRO",
-                "Boeing",
-                "Viasat",
-                "NASA",
-                "Astra",
-                "Mars Colony"
+              "SPCE", "RKLB", "ASTR", "GALT", "THRX",
+  "SPACEX", "BLUEORIGIN", "ASTRA", "MAXAR", "BOEING",
+  "LUNARMINING", "MARSINC", "ASTROFUEL", "STARLINK", "GALAXYCREDIT",
+  "COSMOCOIN", "ORBITX", "NEBULA", "QUASAR", "SATURNBANK",
+  "PLUTOGOLD", "MARSFOOD", "JUPITERJET", "METEORX", "ASTROWATER",
+  "SPACEDRIVE", "DEEPSKY", "EXOGROWTH", "COSMOMINING", "NEOSTOCK"
               ]}
               typeSpeed={150}
               backSpeed={90}
@@ -51,22 +49,14 @@ const Hero = () => {
                             className="rounded-md bg-indigo-600 px-5 py-3 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-600">
                             Get started
                         </Link>
-                        <a href="#" className="text-sm font-semibold text-zinc-200">
-                            Learn more <span aria-hidden="true">&rarr;</span>
-                        </a>
                     </div>
-                
                 </div>
-               <div className="flex mt-10 mb - 20 justify-center">
-                <video autoPlay muted loop className="rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video1} type='video/mp4'/>
-                </video>
-                <video autoPlay muted loop className="rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video2} type='video/mp4'/>
-                </video>
-               </div>
+                <div className="flex mt-10 mb - 20 justify-center max-w-1/3">
+                      <video autoPlay loop  className="h-300 w-2/3 rounded-l mx-2 my-4">
+                                <source src={video1} type='video/mp4'/>
+                                </video></div>
                 </div>
         );
 }
 
-export default Hero
+export default Hero;

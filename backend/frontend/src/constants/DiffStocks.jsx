44c66173a0090a6ab6d46157e0d15605d2f90519
varun99015 <153973261+varun99015@users.noreply.github.com>
2025-08-22 21:45:05 +0530

    Delete backend directory

--- backend/frontend/src/constants/DiffStocks.jsx
+++
@@ -1,8 +0,0 @@
-
-export const Stocks =[
-   {name : 'spaceStocks', },
-   {name : 'satelliteStocks', },
-   {name : 'spaceFinanceStocks', },
-   {name : 'miningStocks',},
-   {name : 'colonizationStocks'}
-];
